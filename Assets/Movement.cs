b578e81c76c33d0b7c8a4f9fe2e340a904206e08
Jaivaan <ivan.renteria@opendeusto.es>
2024-05-17 08:39:54 +0200

    player movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,30 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.AI;
 
 public class movement : MonoBehaviour
 {
-    public Transform goal; // Reference to the goal object
-
     private NavMeshAgent agent; // Reference to the NavMeshAgent component
 
     // Start is called before the first frame update
     void Start()
-
     {
         agent = GetComponent<NavMeshAgent>(); // Get the NavMeshAgent component
-
+/*
         if (goal != null)
         {
             agent.SetDestination(goal.position); // Set the destination to the goal position
-        }
+        }*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        // You can add any additional logic here if needed
+        if (Input.GetMouseButtonDown(0)) // Check if left mouse button was clicked
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Create a ray from the camera to the point where the mouse was clicked
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) // Check if the ray hit something
+            {
+                agent.SetDestination(hit.point); // Set the destination to the point where the ray hit
+            }
+        }
     }
 }
